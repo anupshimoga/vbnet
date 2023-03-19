9f8bb5a13cf038794e18a44b4580e31799d55aa5
Anup Shimoga <anups141988@gmail.com>
2023-03-18 19:37:22 -0700

    Added Calculator

---
+++ Calculator/Form1.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtFirst = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.txtSecond = New System.Windows.Forms.TextBox()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtFirst
+        '
+        Me.txtFirst.Location = New System.Drawing.Point(61, 137)
+        Me.txtFirst.Name = "txtFirst"
+        Me.txtFirst.Size = New System.Drawing.Size(101, 22)
+        Me.txtFirst.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(289, 46)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(132, 64)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "-"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(289, 150)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(132, 66)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "+"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(289, 249)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(132, 64)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "x"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'txtSecond
+        '
+        Me.txtSecond.Location = New System.Drawing.Point(584, 137)
+        Me.txtSecond.Name = "txtSecond"
+        Me.txtSecond.Size = New System.Drawing.Size(126, 22)
+        Me.txtSecond.TabIndex = 4
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Location = New System.Drawing.Point(280, 373)
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.Size = New System.Drawing.Size(155, 22)
+        Me.txtOutput.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(58, 104)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(109, 16)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Enter 1st Number"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(581, 104)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(114, 16)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Enter 2nd Number"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(332, 345)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 16)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Output"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.txtSecond)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtFirst)
+        Me.Name = "Form1"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtFirst As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents txtSecond As TextBox
+    Friend WithEvents txtOutput As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Calculator/Form1.vb
@@ -0,0 +1,45 @@
+﻿Public Class Form1
+    Dim firstNum, secondNum, resultNum As Double
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If txtFirst.Text <> "" And txtSecond.Text <> "" Then
+            firstNum = CDbl(txtFirst.Text)
+            secondNum = CDbl(txtSecond.Text)
+            resultNum = firstNum - secondNum
+            txtOutput.Text = resultNum
+        Else
+            MessageBox.Show("Please enter the both numbers")
+        End If
+
+    End Sub
+
+    Private Sub txtFirst_TextChanged(sender As Object, e As EventArgs) Handles txtFirst.TextChanged
+        txtOutput.Clear()
+    End Sub
+
+    Private Sub txtSecond_TextChanged(sender As Object, e As EventArgs) Handles txtSecond.TextChanged
+        txtOutput.Clear()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If txtFirst.Text <> "" And txtSecond.Text <> "" Then
+            firstNum = CDbl(txtFirst.Text)
+            secondNum = CDbl(txtSecond.Text)
+            resultNum = firstNum + secondNum
+            txtOutput.Text = resultNum
+        Else
+            MessageBox.Show("Please enter the both numbers")
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If txtFirst.Text <> "" And txtSecond.Text <> "" Then
+            firstNum = CDbl(txtFirst.Text)
+            secondNum = CDbl(txtSecond.Text)
+            resultNum = firstNum * secondNum
+            txtOutput.Text = resultNum
+        Else
+            MessageBox.Show("Please enter the both numbers")
+        End If
+    End Sub
+End Class
